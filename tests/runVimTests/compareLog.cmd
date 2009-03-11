fd4ab0cb86e57349527dc021a0f476186f24f86f
Ingo Karkat <swdev@ingo-karkat.de>
2009-03-12 00:00:00 +0100

    The test file (suite) is now also embedded in the captured output name so that multiple test files and suites can be captured.

--- tests/runVimTests/compareLog.cmd
+++ tests/runVimTests/compareLog.cmd
@@ -8,23 +8,30 @@
 ::*
 ::*******************************************************************************
 ::* CONTENTS: 
-::  Runs the runVimTests self-test suite and compares the test output with
-::  previously captured nominal output. 
-::  The command-line arguments to runVimTests are embedded in the captured
-::  output filename and are extracted automatically: 
-::  testrun-1-v.log -> $ runVimTests -1 -v . > /tmp/testrun-1-v.log
+::  Runs a runVimTests self-test and compares the test output with previously
+::  captured nominal output. 
+::  The command-line arguments to runVimTests and the test file are embedded in
+::  the captured output filename and are extracted automatically: 
+::  testrun.suite-1-v.log ->
+::	$ runVimTests -1 -v testrun.suite > /tmp/testrun.suite-1-v.log
+::  The special name "testdir" represents all tests in the directory (i.e. '.'): 
+::  testdir-1-v.log ->
+::	$ runVimTests -1 -v . > /tmp/testdir-1-v.log
 ::       	
 ::* REMARKS: 
 ::       	
 ::* REVISION	DATE		REMARKS 
+::	003	07-Mar-2009	The test file (suite) is now also embedded in
+::				the captured output name so that multiple test
+::				files and suites can be captured. 
 ::	002	25-Feb-2009	Command-line arguments are now embedded in the
 ::				captured output filename and extracted
 ::				automatically. 
 ::	001	11-Feb-2009	file creation
 ::*******************************************************************************
 
-if "%~1" == "" (set old=testrun.log) else (set old=%~1)
-if "%~1" == "" (set log=%TEMP%\testrun.log) else (set log=%TEMP%\%~nx1)
+if "%~1" == "" (set old=testdir.log) else (set old=%~1)
+if "%~1" == "" (set log=%TEMP%\testdir.log) else (set log=%TEMP%\%~nx1)
 if exist "%log%" del "%log%" || exit /B 1
 
 if not exist "%old%" (
@@ -33,15 +40,25 @@ if not exist "%old%" (
 )
 
 set options=
+set tests=testdir
 if "%~1" == "" (goto:run)
+set tests=
 set options=%~n1
-set options=%options:testrun=%
+
+:cutOffTests
+if "%options%" == "" (goto:run)
+set tests=%tests%%options:~0,1%
+set options=%options:~1%
+if "%options%" == "" (goto:run)
+if not "%options:~0,1%" == "-" (goto:cutOffTests)
+
 set options=%options:--= !!%
 set options=%options:-= !%
 set options=%options:!=-%
 
 :run
-call runVimTests.cmd%options% . > "%log%"
+if "%tests%" == "testdir" set tests=.
+call runVimTests.cmd%options% "%tests%" > "%log%"
 
 echo.
 echo.DIFFERENCES:
