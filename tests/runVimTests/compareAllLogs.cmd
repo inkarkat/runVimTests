a6bce1311ecbc59e14a21eaa271d72b8457502d5
Ingo Karkat <swdev@ingo-karkat.de>
2012-07-27 00:00:00 +0200

    file creation

---
+++ tests/runVimTests/compareAllLogs.cmd
@@ -0,0 +1,24 @@
+@echo off %debug%
+::/*************************************************************************/^--*
+::**
+::* FILE: 	compareAllLogs.cmd
+::* PRODUCT:	runVimTests
+::* AUTHOR: 	Ingo Karkat <ingo@karkat.de>
+::* DATE CREATED:   07-Mar-2009
+::*
+::*******************************************************************************
+::* CONTENTS: 
+::  Runs all existing runVimTests self-test suites. 
+::       	
+::* REMARKS: 
+::       	
+::* REVISION	DATE		REMARKS 
+::	001	07-Mar-2009	file creation
+::*******************************************************************************
+
+for %%f in ("%~dp0*.log") do (
+    (echo.)
+    (echo.%%~nxf)
+    call "%~dp0compareLog.cmd" "%%~f"
+)
+
